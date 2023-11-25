2d321f92fa1f9379694c226b852e88e19ba73539
Swasti008 <144793835+Swasti008@users.noreply.github.com>
2023-11-26 05:23:13 +0530

    Ca DONE

--- instruction.js
+++ instruction.js
@@ -3,7 +3,7 @@ let next = document.getElementById("nextButton");
 next.addEventListener("click",function(){
     window.location.href="./enter.html";
 })
-//audio
+//for adding background music
 document.addEventListener('DOMContentLoaded', function() {
     var audio = new Audio("./assets/audio_bgMusic.mp3");
     audio.loop = true;
